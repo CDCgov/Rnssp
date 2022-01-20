a32abd8d3096e3764279e4880883c4c853ee3c7a
rosericazondekon <poj2@cdc.gov>
2022-01-20 11:32:07 -0500

    reorganize rstudio addins functions

--- R/rnssp_gui.R
+++ R/rnssp_gui.R
@@ -26,7 +26,8 @@ add_rmd_template_gui <- function() {
                   checkboxesChecked.push(checkboxes[i].value);
                 }
               }
-              Shiny.onInputChange("checked_rows",checkboxesChecked);  })')
+              Shiny.onInputChange("checked_rows",checkboxesChecked);  })'
+            )
         )
       )
     )
@@ -138,6 +139,57 @@ add_rmd_template_gui <- function() {
       }
       for(templ in input$checked_rows){
         Rnssp::add_rmd_template(templ)
+      }
+      shiny::stopApp()
+    })
+
+    shiny::observeEvent(input$cancel, {
+      shiny::stopApp()
+    })
+
+  }
+
+  viewer <- shiny::dialogViewer("Add")
+  shiny::runGadget(ui, server, viewer = viewer)
+
+}
+
+
+#' Remove Rnssp templates
+#'
+#' Wrapper around \code{\link[=remove_rmd_template]{remove_rmd_template()}}
+#' to Remove Existing NSSP RMarkdown Templates.
+#'
+#' @keywords internal
+#'
+remove_rmd_template_gui <- function() {
+  templates <- basename(
+    list.dirs(
+      file.path(
+        system.file(package = "Rnssp"), "rmarkdown/templates"), recursive = FALSE)
+  )
+
+  ui <- miniUI::miniPage(
+    miniUI::gadgetTitleBar(
+      "Remove Rnssp RMD Templates",
+      right = miniUI::miniTitleBarButton("done", "Remove", primary = TRUE)
+    ),
+    miniUI::miniContentPanel(
+      shiny::checkboxGroupInput("templ",
+                                label = "Existing templates",
+                                inline = TRUE,
+                                choices = templates)
+    )
+  )
+
+  server <- function(input, output, session) {
+
+    shiny::observeEvent(input$done, {
+      if(is.null(input$templ)){
+        shiny::stopApp()
+      }
+      for(templ in input$templ){
+        Rnssp::remove_rmd_template(templ)
       }
       shiny::stopApp()
     })
