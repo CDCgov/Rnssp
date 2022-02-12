0e137628ac5fb852094895c2bf94f54ac85bb323
rosericazondekon <poj2@cdc.gov>
2022-02-12 00:56:07 -0500

    Rewrite test scripts, add missing helper function.

--- tests/testthat/test-alert_regression.R
+++ tests/testthat/test-alert_regression.R
@@ -5,7 +5,7 @@ test_that("alert_regression functions as expected!", {
     date = seq.Date(as.Date("2020-01-01"), as.Date("2020-12-31"), by = 1),
     count = floor(runif(366, min = 0, max = 101))
   )
-  df_regression <- alert_regression(df)
+  df_regression <- alert_regression(df, t = date, y = count)
 
   expect(is.data.frame(df_regression), failure_message = "alert_regression test fails!")
   expect_equal(length(df_regression), length(df) + 5)
