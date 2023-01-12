d53739d1c087c29ad3eabf38ebc36ebfcda76fca
rosericazondekon <poj2@cdc.gov>
2023-01-12 10:24:05 -0500

    Update package test scripts

--- tests/testthat/test-remove_rmd_template.R
+++ tests/testthat/test-remove_rmd_template.R
@@ -1,6 +1,7 @@
 context("test-remove_rmd_template")
 
 test_that("remove_rmd_template() works!", {
+  add_rmd_template("text_mining", restart = FALSE)
+  expect_message(remove_rmd_template("text_mining", restart = FALSE))
   expect_error(remove_rmd_template("text_mining", restart = FALSE))
-  expect_message(remove_rmd_template("state_ed_report", restart = FALSE))
 })
