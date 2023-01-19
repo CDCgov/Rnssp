85727f836f524cfd3ce780d521733bc9286be644
Azondekon <poj2@cdc.gov>
2023-01-19 14:46:11 -0500

    Update run_app function

--- R/run_app.R
+++ R/run_app.R
@@ -18,8 +18,7 @@ run_app <- function(app, launch_browser = TRUE) {
   if (missing(app) || !nzchar(app) || !app %in% available_apps) {
     available_apps_msg %>%
       paste0("Please execute {.fn run_app} with a valid app name as an argument.\n", .) %>%
-      cli::cli_alert_info()
-    return(NULL)
+      cli::cli_abort()
   }
 
   app_dir <- system.file("shiny-apps", app, package = "Rnssp")
