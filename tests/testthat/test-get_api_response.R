f4fabdc26ecf4301606ac9c1f06c84ea295327fb
Azondekon <poj2@cdc.gov>
2023-01-23 14:30:28 -0500

    Add new functionality to create profile using token

--- tests/testthat/test-get_api_response.R
+++ tests/testthat/test-get_api_response.R
@@ -3,8 +3,17 @@ context("test-get_api_response")
 test_that("get_api_response() function works!", {
   url <- "http://httpbin.org/json"
   handle <- Credentials$new("", "")
+  handle2 <- Token$new("abc1234567890")
+
   expect_s3_class(handle, "R6")
   expect_s3_class(handle, "NSSPCredentials")
+
+  expect_s3_class(handle2, "R6")
+  expect_s3_class(handle2, "NSSPCredentials")
+
   response <- get_api_response(url, profile = handle)
+  response2 <- get_api_response(url, profile = handle2)
+
   expect_s3_class(response, "response")
+  expect_s3_class(response2, "response")
 })
