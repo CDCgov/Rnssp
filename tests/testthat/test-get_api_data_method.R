56e755b4d15a4ef40f2c5c919b3e3eee36340170
Azondekon <poj2@cdc.gov>
2023-01-24 14:10:20 -0500

    Update test scripts

--- tests/testthat/test-get_api_data_method.R
+++ tests/testthat/test-get_api_data_method.R
@@ -2,9 +2,24 @@ context("test-get_api_data_method")
 
 test_that("$get_api_data() method works!", {
   url <- "http://httpbin.org/json"
+  url2 <- "http://httpbin.org/robots.txt"
+
   handle <- Credentials$new("", "")
+  handle2 <- Token$new("abc1234567890")
+
   expect_s3_class(handle, "R6")
   expect_s3_class(handle, "NSSPCredentials")
+  expect_s3_class(handle2, "R6")
+  expect_s3_class(handle2, "NSSPToken")
+
   data <- handle$get_api_data(url)
+  data2 <- handle$get_api_data(url2, fromCSV = TRUE)
+
+  data3 <- handle2$get_api_data(url)
+  data4 <- handle2$get_api_data(url2, fromCSV = TRUE)
+
   expect_type(data, "list")
+  expect_s3_class(data2, "data.frame")
+  expect_type(data3, "list")
+  expect_s3_class(data4, "data.frame")
 })
--- tests/testthat/test-get_api_graph.R
+++ tests/testthat/test-get_api_graph.R
@@ -2,8 +2,19 @@ context("test-get_api_graph")
 
 test_that("get_api_graph() function works!", {
   url <- "http://httpbin.org/image/png"
+
   handle <- Credentials$new("", "")
+  handle2 <- Token$new("abc1234567890")
+
   graph <- get_api_graph(url, profile = handle)
+  graph2 <- handle$get_api_graph(url)
+  graph3 <- get_api_graph(url, profile = handle2)
+  graph4 <- handle2$get_api_graph(url)
+
   expect_s3_class(graph$api_response, "response")
+  expect_s3_class(graph2$api_response, "response")
+  expect_s3_class(graph3$api_response, "response")
+  expect_s3_class(graph4$api_response, "response")
+
   expect_error(expect_message(get_api_graph(url, profile = list())))
 })
