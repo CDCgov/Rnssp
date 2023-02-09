ec3d5959856fc5ef7114a63bb2f91ebb2fc914c9
Azondekon <poj2@cdc.gov>
2023-02-09 15:26:24 -0500

    Add new version detection and upgrade suggestion messages

--- R/zzz.R
+++ R/zzz.R
@@ -14,4 +14,21 @@ Rnssp RMD Templates Documentation: https://cdcgov.github.io/Rnssp-rmd-templates
 
 Run 'Rnssp_vignettes()' to browse all Rnssp vignettes.
 ")
+  url <- paste0("https://raw.githubusercontent.com/cdcgov/Rnssp/master/DESCRIPTION")
+
+  x <- suppressWarnings(try(readLines(url), silent = TRUE))
+  remote_version <- gsub("Version:\\s*", "", x[grep('Version:', x)])
+  installed_version <- tryCatch(packageVersion(gsub(".*/", "", "Rnssp")), error=function(e) NA)
+
+  if (all(remote_version > installed_version, class(x) != "try-error")) {
+    message(
+      paste0(
+        '## Rnssp v',
+        installed_version,
+        ' is outdated!\n## Please upgrade to latest Rnssp v',
+        remote_version,
+        ': devtools::install_github("cdcgov/Rnssp")')
+    )
+  }
 }
+
