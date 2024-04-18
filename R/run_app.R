14afcabb24e09819e6b1ef5517a53aadd03a67f9
Roseric Azondekon <roseric_2000@yahoo.fr>
2024-04-18 09:38:02 -0400

    Update run_app() for apps to run from temp dirs

--- R/run_app.R
+++ R/run_app.R
@@ -13,37 +13,37 @@ run_app <- function(app, launch_browser = TRUE) {
   local_apps <- list.files(apps_dir)
   remote_apps <- Rnssp::list_apps()
   available_apps <- unique(c(local_apps, remote_apps))
-  
+
   available_apps_msg <- paste0(
     "Available apps are: ",
     paste0("{.var ", available_apps, "}", collapse = "', ")
   )
-  
+
   if(missing(app) || !nzchar(app) || !app %in% available_apps) {
     cli::cli_abort(
       paste0(
-        "Please execute {.fn run_app} with a valid app name as an argument.\n", 
+        "Please execute {.fn run_app} with a valid app name as an argument.\n",
         available_apps_msg
       )
     )
   }
-  
+
   if(app %in% local_apps){
     app_dir <- system.file("shiny-apps", app, package = "Rnssp")
     suppressWarnings(
       shiny::runApp(
-        app_dir, 
-        display.mode = "normal", 
+        app_dir,
+        display.mode = "normal",
         launch.browser = launch_browser
       )
     )
   } else {
     shiny::runGitHub(
-      repo = "Rnssp-shiny-apps", 
+      repo = "Rnssp-shiny-apps",
       username = "cdcgov",
-      subdir = app, 
-      destdir = apps_dir, 
-      ref = "master", 
+      subdir = app,
+      destdir = tempdir(),
+      ref = "master",
       launch.browser = launch_browser
     )
   }
