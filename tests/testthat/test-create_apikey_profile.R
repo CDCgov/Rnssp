67116d0fa52efe5423987151fbc6d1e7e6ead827
Azondekon <poj2@cdc.gov>
2024-06-25 15:46:29 -0400

    Update test for create_apikey_profile() function

--- tests/testthat/test-create_apikey_profile.R
+++ tests/testthat/test-create_apikey_profile.R
@@ -5,4 +5,9 @@ test_that("create_apikey_profile() works!", {
   testthat::expect_type(myProfile, "environment")
   testthat::expect_s3_class(myProfile, "R6")
   testthat::expect_s3_class(myProfile, "NSSPApikey")
+
+  myProfile2 <- create_apikey_profile("abc1234567890", key_name = "key")
+  testthat::expect_type(myProfile2, "environment")
+  testthat::expect_s3_class(myProfile2, "R6")
+  testthat::expect_s3_class(myProfile2, "NSSPApikey")
 })
