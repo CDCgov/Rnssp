82761bf743a3cb4d5aac101a80f0f60e66e6aa19
Azondekon <poj2@cdc.gov>
2024-06-27 14:57:03 -0400

    Update class Credentials

--- R/credentials.R
+++ R/credentials.R
@@ -57,38 +57,6 @@ Credentials <- R6::R6Class(
     },
 
     #' @description
-    #' Get API data
-    #' @param url a character ofAPI URL
-    #' @param fromCSV a logical, defines whether data are returned in .csv format or .json format
-    #' @param ... further arguments and CSV parsing parameters to be passed to \code{\link[readr]{read_csv}} when \code{fromCSV = TRUE}.
-    #' @return a dataframe (\code{fromCSV = TRUE}) or a list containing a dataframe and its metadata (\code{fromCSV = TRUE})
-    #' @examples
-    #' \dontrun{
-    #' myProfile <- Credentials$new(askme("Enter my username: "), askme())
-    #' json_url <- "https://httpbin.org/json"
-    #' api_data_json <- myProfile$get_api_data(json_url)
-    #'
-    #' csv_url <- "https://httpbin.org/robots.txt"
-    #' api_data_csv <- myProfile$get_api_data(csv_url, fromCSV = TRUE)
-    #' }
-    # get_api_data = function(url, fromCSV = FALSE, ...) {
-    #   assertions::assert_string(url)
-    #   apir <- self$get_api_response(url)
-    #   if(apir$status_code == 200){
-    #     if(any("data.frame" %in% class(httr::content(apir, as = "text")))){
-    #       return(httr::content(apir, as = "text"))
-    #     }
-    #     apir %>% {
-    #       if (fromCSV) {
-    #         httr::content(., by = "text/csv") %>% readr::read_csv(...)
-    #       } else {
-    #         httr::content(., as = "text") %>% jsonlite::fromJSON()
-    #       }
-    #     }
-    #   }
-    # },
-
-    #' @description
     #' Get API graph
     #' @param url a character of API URL
     #' @param file_ext a non-empty character vector giving the file extension. Default is \code{.png}.
