814792cd2f1e010560d08cf4f4a6e23eaf04889b
Azondekon <poj2@cdc.gov>
2024-06-27 14:22:15 -0400

    Update class Token

--- R/token.R
+++ R/token.R
@@ -1,17 +1,17 @@
 #' A \code{Token} Class Representing a Token object
 #'
 #' @description
-#' A \code{Token} object has a token string and a key.
+#' A \code{Token} object has a token string and an authentication type.
 #'
 #' @details
 #' A \code{Token} object can get API data via an API URL.
 
 Token <- R6::R6Class(
   "NSSPToken",
+  inherit = Auth,
   private = list(
     ..token = NSSPContainer$new(NULL),
-    ..auth_type = NULL,
-    ..__ = NSSPContainer$new(stringi::stri_rand_strings(1, 1024, pattern = "[A-Za-z0-9*-+=/_$@.?!%|;:#~<>()[]\`\']"))
+    ..auth_type = NULL
   ),
   public = list(
 
@@ -53,35 +53,6 @@ Token <- R6::R6Class(
         res$request$options$auth_token <- ""
         cli::cli_alert_info(httr::http_status(res$status_code)$message)
         return(res)
-      }
-    },
-
-    #' @description
-    #' Get API data
-    #' @param url a character ofAPI URL
-    #' @param fromCSV a logical, defines whether data are returned in .csv format or .json format
-    #' @param ... further arguments and CSV parsing parameters to be passed to \code{\link[readr]{read_csv}} when \code{fromCSV = TRUE}.
-    #' @return a dataframe (\code{fromCSV = TRUE}) or a list containing a dataframe and its metadata (\code{fromCSV = TRUE})
-    #' @examples
-    #' \dontrun{
-    #' myProfile <- Token$new("abc1234567890")
-    #' json_url <- "https://httpbin.org/json"
-    #' api_data_json <- myProfile$get_api_data(json_url)
-    #'
-    #' csv_url <- "https://httpbin.org/robots.txt"
-    #' api_data_csv <- myProfile$get_api_data(csv_url, fromCSV = TRUE)
-    #' }
-    get_api_data = function(url, fromCSV = FALSE, ...) {
-      assertions::assert_string(url)
-      apir <- self$get_api_response(url)
-      if(apir$status_code == 200){
-        apir %>% {
-          if (fromCSV) {
-            httr::content(., by = "text/csv") %>% readr::read_csv(...)
-          } else {
-            httr::content(., as = "text") %>% jsonlite::fromJSON()
-          }
-        }
       }
     },
 
