b12a34c6d24c9a708e5b03bdeb8f3f83f4fdef86
Azondekon <poj2@cdc.gov>
2024-06-27 14:27:02 -0400

    Update Auth class

--- R/auth.R
+++ R/auth.R
@@ -11,33 +11,11 @@ Auth <- R6::R6Class(
   public = list(
 
     #' @description
-    #' Get API response
-    #' @param url a character of API URL
-    #' @return An object of class \code{response}
-    #' @examples
-    #' \dontrun{
-    #' myAuth <- Auth$new()
-    #' url <- "https://httpbin.org/json"
-    #' api_response <- myAuth$get_api_response(url)
-    #' }
-    get_api_response = function(url) {
-    },
-
-    #' @description
     #' Get API data
-    #' @param url a character ofAPI URL
+    #' @param url a character of API URL
     #' @param fromCSV a logical, defines whether data are returned in .csv format or .json format
     #' @param ... further arguments and CSV parsing parameters to be passed to \code{\link[readr]{read_csv}} when \code{fromCSV = TRUE}.
     #' @return a dataframe (\code{fromCSV = TRUE}) or a list containing a dataframe and its metadata (\code{fromCSV = TRUE})
-    #' @examples
-    #' \dontrun{
-    #' myAuth <- Auth$new()
-    #' json_url <- "https://httpbin.org/json"
-    #' api_data_json <- myAuth$get_api_data(json_url)
-    #'
-    #' csv_url <- "https://httpbin.org/robots.txt"
-    #' api_data_csv <- myAuth$get_api_data(csv_url, fromCSV = TRUE)
-    #' }
     get_api_data = function(url, fromCSV = FALSE, ...) {
       assertions::assert_string(url)
       apir <- self$get_api_response(url)
